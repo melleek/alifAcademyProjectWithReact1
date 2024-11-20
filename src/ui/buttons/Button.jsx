cdb5e830cbdde4b6d7c515786722fc40a7986c07
Anushervon Abdusamadov <anushervon.a14@gmail.com>
2024-11-21 00:52:05 +0500

    add sections

---
+++ src/ui/buttons/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Buttons.css";
+import triangle from "../../assets/icons/chevron_right.svg";
+
+function ButtonS() {
+    return (
+        <div className="btns">
+            <button className="firstBtn">Explore properties</button>
+            <button className="secondBtn">
+                Contact agent <img style={{marginLeft: "5px"}} src={triangle} alt="arrow" />
+            </button>
+        </div>
+    );
+}
+
+export default ButtonS;
