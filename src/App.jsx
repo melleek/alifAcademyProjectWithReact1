676832cf8e4f535a414e656a1d6ff3605dc25b24
Angel <jamshedzodafarishtamoh@gmail.com>
2024-11-22 01:26:10 +0500

    fixed isseu in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useEffect } from "react";
 import "./App.css";
 import Footer from "./footer/Footer";
 import { Sections } from "./sections/Sections";
 
 function App() {
-  useEffect(() => {
-    console.log("salom");
-  }, []);
   return (
     <>
-      <h1>salom</h1>
       <Sections />
       <Footer />
     </>
